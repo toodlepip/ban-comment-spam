69f344cb0a84b7a280c430a7ee0dbea8b65166aa
Sam Michel <sam@chinwag.com>
2013-11-27 02:16:37 +0000

    WIP iptables

--- ban-them.py
+++ ban-them.py
@@ -255,7 +255,7 @@ def main(argv=None):
             # Create blacklist set, will fail gracefully if it exists already
             subprocess.check_call(["echo", "ipset --create blacklist iphash --hashsize 4096"])
             
-            p = subprocess.Popen(["iptables", "-L -n"], stdout=subprocess.PIPE).communicate()[0]
+            p = subprocess.Popen(["iptables", "-L", "-n"], stdout=subprocess.PIPE).communicate()[0]
             print p
             match = re.search('-A INPUT -m set --match-set blacklist src -j DROP', p)
             if not match:
